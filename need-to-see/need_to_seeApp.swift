1cd2117368d99ec0ed954c1067c03394ca508b9a
Bruno Duarte Pereira Santos <duartebrunops@gmail.com>
2023-12-26 17:39:53 -0300

    Initial Commit

---
+++ need-to-see/need_to_seeApp.swift
@@ -0,0 +1,17 @@
+//
+//  need_to_seeApp.swift
+//  need-to-see
+//
+//  Created by Bruno Duarte on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct need_to_seeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
